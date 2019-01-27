02967f841489c08f4a08ddd7b9972a6f0fbebb76
JEToStack <vichethchanrith@gmail.com>
2019-01-28 02:09:21 +0700

    -Modified activity_main layout by adding navigation drawer as root layout -Create and modified activity_nav_header layout -Create menu items to use in navigation view -Add some of more icons to use in navigation drawer

--- app/src/main/java/com/example/molika/joulptes/MainActivity.java
+++ app/src/main/java/com/example/molika/joulptes/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.molika.joulptes;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
